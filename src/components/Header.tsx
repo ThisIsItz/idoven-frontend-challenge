8325e3a7cc67606d18df3230b425c7f4d889f5db
Itziar <itziar@Itziars-MacBook-Air.local>
2023-09-28 18:06:30 +0900

    feat(basic-setup): improve basic Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 
 export default function Header() {
-    return <Box sx={{fontSize: '40px'}}>Idoven.ai Coding Challenge</Box>
+    return <Box><h1>Idoven.ai Coding Challenge</h1></Box>
 }
 
