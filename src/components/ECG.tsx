dd1f6b136c3ca046adb9521bfe1252d8397e3091
Itziar Urbieta <itziarurbieta94@gmail.com>
2023-09-30 19:46:32 +0900

    feat(ecg-chart): add Loader to ECG file and improve code

--- src/components/ECG.tsx
+++ src/components/ECG.tsx
@@ -1,31 +1,22 @@
-import { Chart } from 'chart.js/auto';
-import { useDataContext } from '../DataContext';
-import ECGChart from './ECGChart';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import { useDataContext } from '../DataContext'
+import ECGChart from './ECGChart'
+import Loader from './Loader'
 
-export default function ECG(){
-    const { data, currentPage, handlePrevPage, handleNextPage } = useDataContext();
+export default function ECG() {
+  const { data, currentPage, handlePrevPage, handleNextPage } = useDataContext()
 
-    console.log('data', data)
-
-
-
-    if(data.length > 1){
-        return (
-            <div>
-              <div>
-  <ECGChart data={data}/>
-</div>
-            <button onClick={handlePrevPage} disabled={currentPage === 1}>
-              Move left
-            </button>
-            <button onClick={handleNextPage}>Move right</button>
-          </div>
-          );
-    }
-    return  <Box sx={{ display: 'flex' }}>
-    <CircularProgress />
-  </Box>
+  if (data.length > 1) {
+    return (
+      <div>
+        <div>
+          <ECGChart data={data} />
+        </div>
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Move left
+        </button>
+        <button onClick={handleNextPage}>Move right</button>
+      </div>
+    )
   }
-
+  return <Loader />
+}
