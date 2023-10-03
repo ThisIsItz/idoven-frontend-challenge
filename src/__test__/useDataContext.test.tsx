0f00f933f4e3b5832baa4a232df35f0d9371406b
Itziar Urbieta <itziarurbieta94@gmail.com>
2023-10-03 11:30:28 +0900

    feat(test): create basic test for useDataContext

---
+++ src/__test__/useDataContext.test.tsx
@@ -0,0 +1,30 @@
+import ECGWrapper from '../components/ECGWrapper'
+import { DataContext } from '../contexts/DataContext'
+import userEvent from '@testing-library/user-event'
+import { render, screen, waitFor } from '@testing-library/react'
+
+describe('useDataContext', () => {
+  const contextData = {
+    data: [
+      { time: 1, value: 2 },
+      { time: 2, value: 10 }
+    ],
+    handleNextPage: jest.fn()
+  }
+
+  test('test if context is working', async () => {
+    render(
+      <DataContext.Provider value={contextData}>
+        <ECGWrapper />
+      </DataContext.Provider>
+    )
+
+    const loadMoreButton = screen.getByText('Load more data')
+
+    await waitFor(() => {
+      userEvent.click(loadMoreButton)
+    })
+
+    expect(contextData.handleNextPage).toHaveBeenCalled()
+  })
+})
